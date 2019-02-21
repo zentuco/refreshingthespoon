e6eefd37533202c9627ad8d973ddf100c0b02c64
Hugo Collado <hugo.collado91@gmail.com>
2019-02-21 13:26:11 +0000

    done practising

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,2 +1,19 @@
 class RestaurantsController < ApplicationController
+  RESTAURANTS = {
+    1 => { name: "Dishoom", address: "Shoreditch, London", category: "indian" },
+    2 => { name: "Sushi Samba", address: "City, London", category: "japanese" }
+  }
+
+  def index
+    @category = params[:food_type]
+    @restaurants = RESTAURANTS.select { |id, r| r[:category] == @category }
+  end
+
+  def show
+    @restaurant = RESTAURANTS[params[:id].to_i]
+  end
+
+  def create
+    render plain: "Add to DB restaurant '#{params[:name]}' with address '#{params[:address]}'"
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,7 @@ Rails.application.routes.draw do
   get 'about', to: 'pages#about', as: :about
   get 'contact', to: 'pages#contact', as: :contact
   get 'restaurants', to: 'restaurants#index'
+  post 'restaurants', to: 'restaurants#create'
+  get 'restaurants/:id', to: 'restaurants#show', as: :restaurant
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
