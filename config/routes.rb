fc205e08e98aee0c741e8d29dff7d382839882d9
Hugo Collado <hugo.collado91@gmail.com>
2019-02-21 12:44:47 +0000

    created Rails app, pages controller, about, contact and home pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  get 'about', to: 'pages#about', as: :about
+  get 'contact', to: 'pages#contact', as: :contact
+  root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
